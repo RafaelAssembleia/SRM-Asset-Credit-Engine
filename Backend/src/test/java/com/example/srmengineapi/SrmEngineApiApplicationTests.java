40e37f462704d307a376ff1bb56f3ec5c01e8fae
Rafael Assembleia <raafar14@gmail.com>
2026-09-13 22:38:51 -0300

    add estrutura backend inicial

---
+++ Backend/src/test/java/com/example/srmengineapi/SrmEngineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.srmengineapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrmEngineApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
